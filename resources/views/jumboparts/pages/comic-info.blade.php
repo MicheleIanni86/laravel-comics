2417d6849c0e7d9baee06049ecc2f6a0d7a49cbe
MicheleIanni86 <micheleianni86@gmail.com>
2024-03-18 16:15:04 +0100

    feat: Added Badges on Images in Info Comics

--- resources/views/jumboparts/pages/comic-info.blade.php
+++ resources/views/jumboparts/pages/comic-info.blade.php
@@ -4,6 +4,8 @@
 <div class="row-blue">
     <div class="container-img-thumb">
         <img src="{{ $comic['thumb'] }}" alt="">
+        <div class="badge-up">COMIC BOOK</div>
+        <div class="badge-down">VIEW GALLERY</div>
     </div>
 
 </div>
